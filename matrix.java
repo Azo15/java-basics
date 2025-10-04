abccb1996811f744516af53f86d51aeeaba80280
Azo İsmail <ismailazo260@gmail.com>
2025-10-04 22:34:55 +0300

    Create matrix.java

---
+++ matrix.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class matrix {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        /*
+        ===================================
+        1) Diziyi ekrana yazdırma (hazır veri)
+        ===================================
+        Burda elimizde hazır bir dizi var.
+        Döngü ile her elemanı yazdırıyoruz.
+         */
+//        int[] myNumbers = {23,343,22,155,76};
+//
+//        for (int n = 0; n < myNumbers.length; n++) {
+//            System.out.print(myNumbers[n] + " ");
+//        }
+
+        /*
+        =========================================
+        2) Kullanıcıdan 5 sayı alıp sıralama (sort)
+        =========================================
+        Kullanıcıdan 5 sayı istiyoruz.
+        Daha sonra Arrays.sort() ile küçükten büyüğe sıralıyoruz.
+         */
+//        int i;
+//        int[] myNumbers = new int[5];
+//
+//        for (i = 0; i < myNumbers.length; i++) {
+//            System.out.println("Enter number " + (i + 1) + ":");
+//            myNumbers[i] = reader.nextInt();
+//        }
+//
+//        Arrays.sort(myNumbers);
+//
+//        for (int k = 0; k < myNumbers.length; k++) {
+//            System.out.print(myNumbers[k] + " ");
+//        }
+
+        /*
+        =====================================
+        3) Kullanıcıdan 5 sayı alıp karesini bulma
+        =====================================
+        Kullanıcıdan 5 tane sayı istiyoruz.
+        Her birinin karesini hesaplayıp ekrana yazdırıyoruz.
+         */
+//        int[] numbers = new int[5];
+//        System.out.println("Enter numbers: ");
+//        for (int i = 0; i < numbers.length; i++) {
+//            numbers[i] = reader.nextInt();
+//        }
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            int square = numbers[i] * numbers[i];
+//            System.out.print(square + " ");
+//        }
+    }
+}
